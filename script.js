6c60f94668047fb3bd66449728bc7e3a54830634
karpikovas <karpikovasophie@gmail.com>
2019-08-28 15:31:08 +0000

    Simple css structure

--- script.js
+++ script.js
@@ -1,207 +1,204 @@
-if (window.addEventListener) {
-  window.addEventListener('load', function () {
-    init();
-  });
-}
+'use strict';
 
-function init() {
+(function () { // pattern "module"
 
-  selectedType = 'circle';
-  selectedShape = null;
-  onMove = false;
+  var container;
+  var btnDelete;
+  var btnIncrease;
+  var btnReduce;
+  var colorPicker;
+
+  var selectedType;
+  var selectedShape;
+  var onMove;
+
+  function init() {
+
+    selectedType = 'circle';
+    selectedShape = null;
+    onMove = false;
+
+    container = document.getElementById('container');
+    btnDelete = document.getElementById('btn-delete');
+    btnIncrease = document.getElementById('btn-increase');
+    btnReduce = document.getElementById('btn-reduce');
+    colorPicker = document.getElementById('color-picker');
+
+    document.getElementById('circle').classList.add('menu__item-circle_active');
 
 
-  container = document.getElementById('container');
-  btnDelete = document.getElementById('btn-delete');
-  btnIncrease = document.getElementById('btn-increase');
-  btnReduce = document.getElementById('btn-reduce');
-  colorPicker = document.getElementById('color-picker');
+    bindEvents();
 
+  }
 
-  container.addEventListener('click', function (e) {
-    paintShape(e);
-  }, false);
-  btnDelete.addEventListener('click', deleteShape, false);
-  btnIncrease.addEventListener('click', increaseShape, false);
-  btnReduce.addEventListener('click', reduceShape, false);
-  colorPicker.addEventListener('change', changeShapeColor, false);
+  function bindEvents() {
+    container.addEventListener('click', function (e) {
+      paintShape(e);
+    }, false);
+    btnDelete.addEventListener('click', deleteShape, false);
+    btnIncrease.addEventListener('click', increaseShape, false);
+    btnReduce.addEventListener('click', reduceShape, false);
+    colorPicker.addEventListener('change', changeShapeColor, false);
 
-  document.getElementById('circle').addEventListener('click', function (e) {
-    onShapeClick(e.target.id);
-  }, false);
-  document.getElementById('rect').addEventListener('click', function (e) {
-    onShapeClick(e.target.id);
-  }, false);
-  document.getElementById('triangle').addEventListener('click', function (e) {
-    onShapeClick(e.target.id);
-  }, false);
+    document.getElementById('circle').addEventListener('click', function (e) {
+      onShapeClick(e.target.id);
+    }, false);
+    document.getElementById('rect').addEventListener('click', function (e) {
+      onShapeClick(e.target.id);
+    }, false);
+    document.getElementById('triangle').addEventListener('click', function (e) {
+      onShapeClick(e.target.id);
+    }, false);
+  }
 
-}
+//------------------Методы кнопок------------------------------------
 
-//Методы кнопок
-
-function onShapeClick(typeOfShape) {
+  function onShapeClick(typeOfShape) {
 
     let oldTypeOfShape = document.getElementById(selectedType);
-    oldTypeOfShape.classList.remove('active');
-    oldTypeOfShape.classList.add('disabled');
+    oldTypeOfShape.classList.remove(`menu__item-${selectedType}_active`);
 
     let newTypeOfShape = document.getElementById(typeOfShape);
-    newTypeOfShape.classList.remove('disabled');
-    newTypeOfShape.classList.add('active');
+    newTypeOfShape.classList.add(`menu__item-${typeOfShape}_active`);
 
     selectedType = typeOfShape;
-}
-
-function changeShapeColor() {
-
-  if (selectedShape.classList.contains('triangle')) {
-    selectedShape.style.borderBottomColor = colorPicker.value;
-  } else {
-    selectedShape.style.background = colorPicker.value;
   }
-}
-function increaseShape() {
+
+  function changeShapeColor() {
+
     if (selectedShape.classList.contains('triangle')) {
-        let newWidth = selectedShape.offsetWidth/2 + 10;
-        selectedShape.style.borderWidth = newWidth + 'px';
-        selectedShape.style.borderBottomWidth = newWidth * 1.7 + 'px';
+      selectedShape.style.borderBottomColor = colorPicker.value;
     } else {
-        selectedShape.style.height = selectedShape.offsetHeight + 10 + 'px';
-        selectedShape.style.width = selectedShape.offsetWidth + 10 + 'px';
+      selectedShape.style.background = colorPicker.value;
     }
-}
+  }
 
-function reduceShape() {
-    if (selectedShape.offsetHeight > 10) {
+  function increaseShape() {
+    let scale = selectedShape.getBoundingClientRect().width / selectedShape.offsetWidth;
+    selectedShape.style.transform = `scale(${scale + 0.1})`;
+  }
 
-
-        if (selectedShape.classList.contains('triangle')) {
-            let newWidth = selectedShape.offsetWidth/2 - 10;
-            selectedShape.style.borderWidth = newWidth + 'px';
-            selectedShape.style.borderBottomWidth = newWidth * 1.7 + 'px';
-        } else {
-            selectedShape.style.height = selectedShape.offsetHeight - 10 + 'px';
-            selectedShape.style.width = selectedShape.offsetWidth - 10 + 'px';
-
-        }
+  function reduceShape() {
+    let scale = selectedShape.getBoundingClientRect().width / selectedShape.offsetWidth;
+    if (scale > 0.1) {
+      selectedShape.style.transform = `scale(${scale - 0.1})`;
     }
-}
-function deleteShape() {
+  }
+
+  function deleteShape() {
     selectedShape.parentNode.removeChild(selectedShape);
     disabledShapeMenu();
     selectedShape = null;
-}
+  }
 
-function paintShape(coords) {
-  if (!onMove) {
-    let top = coords.pageY;
-    let left = coords.pageX;
+  function paintShape(coords) {
+    if (!onMove) {
 
-    let div = document.createElement('div');
-    div.classList.add(selectedType);
-    container.appendChild(div);
+      let top = coords.pageY;
+      let left = coords.pageX;
 
-    div.style.left = left - div.offsetWidth / 2 + 'px';
-    div.style.top = top - div.offsetHeight / 2 + 'px';
+      let div = document.createElement('div');
+      div.classList.add('shape');
+      div.classList.add(`shape__${selectedType}`);
+      container.appendChild(div);
 
-    div.onmousedown = function (event) {
+      div.style.left = left - div.offsetWidth / 2 + 'px';
+      div.style.top = top - div.offsetHeight / 2 + 'px';
 
-      event.stopPropagation();
-      event.preventDefault();
+      div.onmousedown = function (event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-      if (selectedShape != null) {
-        selectedShape.classList.remove('active');
-      } else {
-        activeShapeMenu();
-      }
+        if (selectedShape != null) {
+          selectedShape.classList.remove('shape_active');
+        } else {
+          activeShapeMenu();
+        }
 
-      selectedShape = div;
-      div.classList.add('active');
-      onMove = true;
+        selectedShape = div;
+        div.classList.add('shape_active');
+        onMove = true;
 
-      colorPicker.value = getShapeColor();
+        colorPicker.value = getShapeColor();
 
-      let shiftX = event.clientX - div.getBoundingClientRect().left;
-      let shiftY = event.clientY - div.getBoundingClientRect().top;
+        let shiftX = event.clientX - div.offsetLeft;
+        let shiftY = event.clientY - div.offsetTop;
 
-      moveAt(event.pageX, event.pageY);
 
-      function moveAt(pageX, pageY) {
-        div.style.left = pageX - shiftX + 'px';
-        div.style.top = pageY - shiftY + 'px';
-      }
+        function moveAt(pageX, pageY) {
 
-      function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-      }
+          div.style.left = pageX - shiftX + 'px';
+          div.style.top = pageY - shiftY + 'px';
+        }
 
-      container.addEventListener('mousemove', onMouseMove);
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
 
-      div.onmouseup = function () {
-        container.removeEventListener('mousemove', onMouseMove);
-        div.onmouseup = null;
+
+        }
+
+        container.addEventListener('mousemove', onMouseMove);
+
+        div.onmouseup = function () {
+
+          container.removeEventListener('mousemove', onMouseMove);
+          div.onmouseup = null;
+        };
       };
 
-    };
-
-    div.ondragstart = function () {
-      return false;
-    };
-  } else {
-    onMove = false;
+      div.ondragstart = function () {
+        return false;
+      };
+    } else {
+      onMove = false;
+    }
   }
-}
 
-function activeShapeMenu() {
-    btnDelete.classList.remove('disabled');
-    btnDelete.classList.add('active');
+//------------Вспомогательные функции-----------------
 
-    btnIncrease.classList.remove('disabled');
-    btnIncrease.classList.add('active');
-
-    btnReduce.classList.remove('disabled');
-    btnReduce.classList.add('active');
-
-    colorPicker.disabled = false;
+  function activeShapeMenu() {
+    btnDelete.removeAttribute('disabled');
+    btnIncrease.removeAttribute('disabled');
+    btnReduce.removeAttribute('disabled');
+    colorPicker.removeAttribute('disabled');
     colorPicker.value = selectedShape ? selectedShape.style.background : '#008000';
-}
+  }
 
-function disabledShapeMenu() {
-    btnDelete.classList.remove('active');
-    btnDelete.classList.add('disabled');
-
-    btnIncrease.classList.remove('active');
-    btnIncrease.classList.add('disabled');
-
-    btnReduce.classList.remove('active');
-    btnReduce.classList.add('disabled');
-
-    colorPicker.disabled = true;
+  function disabledShapeMenu() {
+    btnDelete.setAttribute('disabled', 'disabled');
+    btnIncrease.setAttribute('disabled', 'disabled');
+    btnReduce.setAttribute('disabled', 'disabled');
+    colorPicker.setAttribute('disabled', 'disabled');
     colorPicker.value = '#808080';
-}
+  }
 
-function rgbToHex(rgb) {
-  return "#" + ((1 << 24) + (parseInt(rgb[0]) << 16) + (parseInt(rgb[1]) << 8) + parseInt(rgb[2])).toString(16).slice(1);
-}
+  function getShapeColor() {
+    let color;
+    if (selectedShape.classList.contains('triangle')) {
+      color = getStyle(selectedShape, 'borderBottomColor');
+    } else {
+      color = getStyle(selectedShape, 'backgroundColor');
+    }
 
+    let rgbArray = color.match(/\d+/g);
+    return rgbToHex(rgbArray);
+  }
 
-function getStyle(el, styleProp) {
-  if (el.currentStyle)
-    return el.currentStyle[styleProp];
-
-  return document.defaultView.getComputedStyle(el, null)[styleProp];
-}
-
-function getShapeColor() {
-  let color;
-  if (selectedShape.classList.contains('triangle')) {
-    color = getStyle(selectedShape, 'borderBottomColor');
-  } else {
-    color = getStyle(selectedShape, 'backgroundColor');
+  function rgbToHex(rgb) {
+    return "#" + ((1 << 24) + (parseInt(rgb[0]) << 16) + (parseInt(rgb[1]) << 8) + parseInt(rgb[2])).toString(16).slice(1);
   }
 
 
-  let rgbArray = color.match(/\d+/g);
-  return rgbToHex(rgbArray);
-}
+  function getStyle(el, styleProp) {
+    if (el.currentStyle)
+      return el.currentStyle[styleProp];
+
+    return document.defaultView.getComputedStyle(el, null)[styleProp];
+  }
+
+
+  window.addEventListener('load', function () {
+    init();
+  });
+
+})();
